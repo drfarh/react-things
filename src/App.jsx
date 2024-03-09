b0fc51e13c407580c8697baf43bbbaf1f3b67c56
Soheli <farhana.engineer@gmail.com>
2024-03-09 12:42:40 -0500

    add branch-4 (JavaScript in JSX with Curly Braces): Challenges-1,2,3.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Bio from './Bio'
 import Gallery from './Gallery'
 import TodoList from './TodoList'
+import TodoListChl2 from './TodoListChl2'
+import TodoListChl3 from './TodoListChl3'
 
 function App() {
 
@@ -23,7 +25,12 @@ function App() {
     {/* Branch-4 */}
     <div>
       <h1>Branch 4 JavaScript in JSX with curly braces </h1>
+      <h2>Challange 1: Fix the code crashes </h2>
       <TodoList />
+      <h2>Challange 2: Extract information into an object</h2>
+      <TodoListChl2 />
+      <h2>Challange 3: Write an expression inside JSX curly braces</h2>
+      <TodoListChl3 />
     </div>
            
     </>
